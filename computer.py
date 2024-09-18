785d50919bb23cc85cce8c6cbd694e008a655038
llevensailor <llevensailor@smith.edu>
2024-09-18 09:55:09 -0400

    Updating code

--- computer.py
+++ computer.py
@@ -1,10 +1,51 @@
 class Computer:
 
     # What attributes will it need?
+    description: str
+    processor_type: str
+    hard_drive_capacity: int
+    memory: int
+    operating_system: str
+    year_made: int
+    price: int
 
     # How will you set up your constructor?
+
     # Remember: in python, all constructors have the same name (__init__)
-    def __init__():
-        pass # You'll remove this when you fill out your constructor
+    def __init__(self, description: str,processor_type: str,hard_drive_capacity: int, memory: int,operating_system: str,year_made: int, price: int):
+        self.description = description
+        self.processor_type = processor_type
+        self.hard_drive_capacity = hard_drive_capacity
+        self.memory = memory
+        self.operating_system = operating_system
+        self.year_made = year_made
+        self.price = price  
 
     # What methods will you need?
+    def update_price(self, new_price: int):
+        self.price = new_price
+        print("Updated price to", self.price)
+    
+    def update_os(self, new_os: str):
+        self.operating_system = new_os
+        print("Updated price to", self.operating_system)
+
+
+
+def main():
+    
+    # First, let's make a computer
+    c = Computer(
+        "Mac Pro (Late 2013)",
+        "3.5 GHc 6-Core Intel Xeon E5",
+        1024, 64,
+        "macOS Big Sur", 2013, 1500
+    )
+    c.update_price(1900)
+    print(c.price)
+
+
+    print(c.description)
+#Only call main() if I am running this program directly 
+if __name__ == "__main__":
+    main()
--- oo_resale_shop.py
+++ oo_resale_shop.py
@@ -1,10 +1,50 @@
+from computer import Computer
 class ResaleShop:
 
     # What attributes will it need?
-
+    inventory : list
     # How will you set up your constructor?
     # Remember: in python, all constructors have the same name (__init__)
-    def __init__():
-        pass # You'll remove this when you fill out your constructor
-
+    def __init__(self):
+        self.inventory = []
     # What methods will you need?
+    def buy(self, computer):
+       if computer not in self.inventory: 
+            self.inventory.append(computer)
+            print("Bought computer")
+       else:
+           print("Computer is already in inventory")
+           
+
+    def sell(self, computer):
+       if computer in self.inventory:
+            self.inventory.remove(computer)
+       else: 
+           print("Computer not in inventory")
+           
+
+
+
+
+    def refurbish(self, computer, new_price, new_os):
+        if computer in self.inventory:
+            computer.update_price(new_price) #calling from computer class
+            computer.update_os(new_os)
+            print("Computer has been refurbushed. The new price is", computer.price, "and new os is" + computer.os)
+        else:
+            print("Computer not in inventory")
+
+
+
+def main():
+    #Make a computer first
+    c = Computer(
+        "Mac Pro (Late 2013)",
+        "3.5 GHc 6-Core Intel Xeon E5",
+        1024, 64,
+        "macOS Big Sur", 2013, 1500
+    )
+    myShop = ResaleShop()
+    myShop.buy(c)
+
+main()
